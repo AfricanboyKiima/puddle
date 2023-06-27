077bf21993d8b1fb9f547eb941a73dc81e766587
Africanboy Kiima <fadhilkiima@gmail.com>
2023-06-27 07:11:39 +0100

    Accessing data from the db using the db abstraction api for my item and Category to be rendered in template

--- core/views.py
+++ core/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from item.models import Category, Item
 
+""""""
+#index page
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6] #6 items to be retrieved from Item object, only the unsold ones
+    categories = Category.objects.all()#retrieve all categories
+    return render(request, 'core/index.html', {'categories':categories,'items':items,})
+
+
+#contact page
+def contact(request):
+    return render(request, 'core/contact.html')
