2cf79c7ea98d1630638e6c7262c674dab9c40b5f
Africanboy Kiima <fadhilkiima@gmail.com>
2023-09-15 14:32:51 +0100

    Added detail view to give us details on each clicked itemé

---
+++ item/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Item
+
+#view to give us details of each item we click in the index page
+def detail(request,pk):#it receives an id(pk) as each item will be found through its pk
+    item = get_object_or_404(Item,pk=pk)
+    related_item = Item.objects.filter(category = item.category, is_sold = False).exclude(pk=pk)
+    return render(request, 'item/detail.html', {'item':item})
