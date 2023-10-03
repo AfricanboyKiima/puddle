ac13f4bd9b64acca69367d5ba8740915a7f6f24e
Africanboy Kiima <fadhilkiima@gmail.com>
2023-10-03 17:52:14 +0100

    Implemented an index view that renders all the items and caterogories data in the index page

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from item.models import Category, Item
 """"""
 #index page
 def index(request):
-    items = Item.objects.filter(is_sold=False)[0:6] #In SQL == SELECT * FROM Item WHERE is_sold = False LIMIT 6  
+    items = Item.objects.filter(is_sold=False)[:] #In SQL == SELECT * FROM Item WHERE is_sold = False LIMIT 6  
     categories = Category.objects.all()#retrieve all categories
     return render(request, 'core/index.html', {'categories':categories,'items':items,})
 
