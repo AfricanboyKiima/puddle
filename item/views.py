b4c1332d51579fcdf5f8513f7752648c10271b2d
Africanboy Kiima <fadhilkiima@gmail.com>
2023-10-21 18:03:56 +0100

    Added code within detail view to show related items on details page of an item

--- item/views.py
+++ item/views.py
@@ -4,5 +4,5 @@ from .models import Item
 #view to give us details of each item we click in the index page
 def detail(request,pk):#it receives an id(pk) as each item will be found through its pk
     item = get_object_or_404(Item,pk=pk)
-    related_item = Item.objects.filter(category = item.category, is_sold = False).exclude(pk=pk)
+    related_items  = Item.objects.filter(category = item.category, is_sold = False).exclude(pk=pk)[0:3]
     return render(request, 'item/detail.html', {'item':item})
