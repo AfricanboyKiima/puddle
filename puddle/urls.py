966bef8004a045798cf0ef4e1f78bc249b895585
Africanboy Kiima <fadhilkiima@gmail.com>
2023-10-21 18:25:04 +0100

    Removed unnecessary import or files

--- puddle/urls.py
+++ puddle/urls.py
@@ -1,7 +1,5 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path
 #The above imports are just for photo illustration but this should never be done in production  
 ############
 from django.contrib import admin
