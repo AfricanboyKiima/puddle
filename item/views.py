78b4c7157c88c40e79a7b125c4da476f530dca63
Africanboy Kiima <fadhilkiima@gmail.com>
2023-10-21 19:01:25 +0100

    Added related_items variable to dictionnary of the  details view

--- item/views.py
+++ item/views.py
@@ -5,4 +5,4 @@ from .models import Item
 def detail(request,pk):#it receives an id(pk) as each item will be found through its pk
     item = get_object_or_404(Item,pk=pk)
     related_items  = Item.objects.filter(category = item.category, is_sold = False).exclude(pk=pk)[0:3]
-    return render(request, 'item/detail.html', {'item':item})
+    return render(request, 'item/detail.html', {'item':item, 'related_items':related_items})
